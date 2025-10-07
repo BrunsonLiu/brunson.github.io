f6fa7fadf17c83e63d42f1dd7bca0dbbe6745c33
BrunsonLiu <1815751961@qq.com>
2025-10-07 22:46:01 +0800

    新建 layout.js 和 page.js，添加主页相关内容

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ textAlign: "center", padding: "100px" }}>
-      <h1>Welcome to Brunson’s Homepage 🌍</h1>
-      <p>Your deployment works perfectly!</p>
-    </main>
-  );
-}
